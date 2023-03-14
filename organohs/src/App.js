aa0615ac699d68a8f6b4dbdb5fc6aba5021bcb59
heliosalesjr <heliosalesjr@gmail.com>
2023-03-14 18:52:52 -0300

    Day 3

--- organohs/src/App.js
+++ organohs/src/App.js
@@ -1,12 +1,20 @@
 
+import { useState } from 'react';
 import Banner from './components/Banner/Banner';
 import Form from './components/Form/Form'
 
 function App() {
+
+  const [members, setMembers] = useState([]);
+
+  const onSave = (member) => {
+    setMembers([...members, member])
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Form />
+      <Form onSave={member => teamMember(member)}/>
     </div>
   );
 }
--- organohs/src/components/Form/Form.js
+++ organohs/src/components/Form/Form.js
@@ -2,8 +2,9 @@ import './Form.css'
 import TextInputs from '../TextInputs/TextInputs';
 import DropDown from '../DropDown/DropDown';
 import ButtonForm from '../ButtonForm/ButtonForm';
+import { useState } from 'react';
 
-const Form = () => {
+const Form = (props) => {
 
     const teams = [
         'Programação',
@@ -15,14 +16,58 @@ const Form = () => {
         'Inovação e Gestão'
     ];
 
+    //os três estados:
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [times, setTimes] = useState('')
+
+    const onSave = (e) => {
+        e.preventDefault()
+        props.teamMember ({
+            nome: nome,
+            cargo: cargo,
+            imagem: imagem,
+            times: times
+        })
+    }
+
     return (
         <section className='form-main'>
             <form  >
                 <h2>Preecha os dados para criar o card do colaborador</h2>
-                <TextInputs obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
-                <TextInputs label="Cargo" placeholder="Digite seu cargo"/>
-                <TextInputs label="Imagem" placeholder="Selecione sua imagem"/>
-                <DropDown label="Times" item={teams}/>
+                <TextInputs 
+                    obrigatorio={true} 
+                    label="Nome" 
+                    placeholder="Digite seu nome"
+                    valor={nome}
+                    altered={valor => setNome(valor)}
+
+                />
+                <TextInputs 
+                    label="Cargo"
+                    obrigatorio={true} 
+                    placeholder="Digite seu cargo"
+                    valor={cargo}
+                    altered={valor => setCargo(valor)}
+
+                />
+                <TextInputs 
+                    label="Imagem" 
+                    placeholder="Selecione sua imagem"
+                    valor={imagem}
+                    altered={valor =>setImagem(valor)}
+                
+                />
+
+                <DropDown 
+                label="Times" 
+                item={teams}
+                valor={times}
+                altered={valor =>setTimes(valor)}
+
+                />
                 <ButtonForm text='Clica, mona'/>
             </form>
         </section>
