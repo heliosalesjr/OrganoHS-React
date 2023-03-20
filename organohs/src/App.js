76f2d6c1a70f4b584a090f870456667c518945c5
heliosalesjr <heliosalesjr@gmail.com>
2023-03-20 18:36:57 -0300

    day 5

--- organohs/src/App.js
+++ organohs/src/App.js
@@ -1,9 +1,49 @@
 
 import { useState } from 'react';
 import Banner from './components/Banner/Banner';
-import Form from './components/Form/Form'
+import Form from './components/Form/Form';
+import Team from './components/Team/Team';
 
 function App() {
+
+  const mainTeams = [
+    {
+      nome: "Programação",
+      mainColor: '#57C278',
+      secondaryColor: '#D9F7E9'
+    },
+    {
+      nome: "Front-End",
+      mainColor: '#82CFFA',
+      secondaryColor: '#E8F8FF'
+    },
+    {
+      nome: 'Data-Science',
+      mainColor: '#A6D157',
+      secondaryColor: '#F0F8E2'
+    },
+    {
+      nome: 'Devops',
+      mainColor: '#E06B69',
+      secondaryColor: '#FDE7E8'
+    },
+    {
+      nome: 'UX e UI Design',
+      mainColor: '#DB6EBF',
+      secondaryColor: '#FAE9F5'
+    },
+    {
+      nome: 'Mobile',
+      mainColor: '#FFBA05',
+      secondaryColor: '#FFF5D9'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      mainColor: '#FF8A29',
+      secondaryColor: '#FFEEDF'
+    }
+    
+  ]
 
   const [members, setMembers] = useState([]);
 
@@ -16,6 +56,9 @@ function App() {
     <div className="App">
       <Banner />
       <Form teamMember={member => newTeamMember(member)}/>
+
+      {mainTeams.map (team => <Team key={team.nome} nome={team.nome} mainColor={team.mainColor} secondaryColor={team.secondaryColor}/>)}
+
     </div>
   );
 }
---
+++ organohs/src/components/Team/Team.js
@@ -0,0 +1,15 @@
+import './Team.css';
+
+const Team = (props) => {
+
+    return (
+
+        <section className='team' style={{backgroundColor: props.secondaryColor}}>
+            <h3 style={{borderColor: props.mainColor}}>{props.nome}</h3>
+        </section>
+
+    )
+
+}
+
+export default Team;
